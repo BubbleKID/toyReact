ccb676d8cd306bec8c6494832b2e4235ac04b335
Mark <fqchenxin@gmail.com>
2020-08-08 18:20:51 +1000

    add component class

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('aaaaa');
+import { ToyReact, Componenet } from "./ToyReact.js";
+
+class MyComponent extends Componenet {
+  render() {
+    return <div><span>asdasdasd</span>Cool</div>
+  }
+}
+
+let a = <MyComponent name="a" id="abc"></MyComponent>
+  
+ToyReact.render(a, document.body);
