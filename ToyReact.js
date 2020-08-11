aa8b0a43e1fc4a80c96cc99edecde8ca82751f4b
Mark <fqchenxin@gmail.com>
2020-08-12 00:09:42 +1000

    add setState and event listener

--- ToyReact.js
+++ ToyReact.js
@@ -3,6 +3,13 @@ class ElementWrapper {
     this.root = document.createElement(type);
   }
   setAttribute(name, value) {
+    if (name.match(/^on([\s\S]+)$/)) {
+      let eventName = RegExp.$1.replace(/^[\s\S]/, s => s.toLocaleLowerCase())
+      this.root.addEventListener(eventName, value);
+    }
+    if (name === "classsName") {
+      name = "class";
+    }
     this.root.setAttribute(name, value);
   }
   appendChild(vchild) {
@@ -11,6 +18,7 @@ class ElementWrapper {
   mountTo(parent) {
     parent.appendChild(this.root);
   }
+ 
 }
 class TextWrapper {
   constructor(content) {
@@ -21,18 +29,39 @@ class TextWrapper {
   }
 }
 
-export class Componenet {
+export class Component {
   constructor() {
     this.children = [];
+    this.props = Object.create(null);
   }
 
   setAttribute(name, value) {
+    this.props[name] = value;
     this[name] = value;
   }
 
   mountTo(parent) {
     let vdom = this.render();
     vdom.mountTo(parent);
+  }
+  setState(state) {
+    let merge = (oldState, newState) => {
+      for (let p in newState) {
+        if (typeof newState[p] === "object") {
+          if (typeof oldState[p] !== "object") {
+            oldState[p] = {};
+          }
+          merge(oldState[p], newState[p]);
+        } else {
+          oldState[p] = newState[p];
+        }
+      }
+    };
+    if (!this.state && state) {
+      this.state = {};
+    }
+    merge(this.state, state);
+    console.log(this.state);
   }
 }
 
@@ -56,7 +85,7 @@ export let ToyReact = {
           insertChildren(child);
         } else {
           if (
-            !(child instanceof Componenet) &&
+            !(child instanceof Component) &&
             !(child instanceof ElementWrapper) &&
             !(child instanceof TextWrapper)
           ) {
--- main.js
+++ main.js
@@ -1,18 +1,46 @@
-import { ToyReact, Componenet } from "./ToyReact.js";
+import { ToyReact, Component } from "./ToyReact.js";
 
-class MyComponent extends Componenet {
+class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <div>
-        <span>asdasdasd</span>
-        <div>
-          {this.children}
-        </div>
-      </div>
-    )
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
-let a = <MyComponent name="a" id="abc"></MyComponent>
-  
-ToyReact.render(a, document.body);
+class Board extends Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+ToyReact.render(<Board />, document.body);
